c91ef26acf8e7ef06d52f6799a0437dd8fa95a7b
John-David Dalton <john.david.dalton@gmail.com>
2014-04-19 10:05:05 -0700

    Add `isError` support.

--- test/npm/index.js
+++ test/npm/index.js
@@ -115,6 +115,7 @@ lodash.isDate = require('lodash.isdate');
 lodash.isElement = require('lodash.iselement');
 lodash.isEmpty = require('lodash.isempty');
 lodash.isEqual = require('lodash.isequal');
+lodash.isError = require('lodash.iserror');
 lodash.isFinite = require('lodash.isfinite');
 lodash.isFunction = require('lodash.isfunction');
 lodash.isNaN = require('lodash.isnan');
